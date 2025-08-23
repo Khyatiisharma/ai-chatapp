f57f1015bcc3d40bdcb936529b7bec19612f2db1
Harshyadav281 <153888371+Harshyadav281@users.noreply.github.com>
2025-08-23 11:36:23 +0530

    updated the server for public acces

--- backend/server.js
+++ backend/server.js
@@ -105,6 +105,6 @@ io.on("connection", (socket) => {
 });
 
 // ✅ Start server
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server is running on port ${port}`);
 });

79b89941c494535c9dc86e5e6d23dbc0b3079f13
Harshyadav281 <153888371+Harshyadav281@users.noreply.github.com>
2025-08-26 10:29:46 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://98.82.202.185:5173", // frontend ka public IP + port
-    credentials: true,                   // cookies, auth headers ke liye
+    origin: "http://localhost:5173", 
+    credentials: true,                   
   })
 );
 

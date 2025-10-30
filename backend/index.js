1fb0d99426208886c54aea6ba59f6529347ad545
khyati <khyatisharma514@gmail.com>
2025-10-30 20:59:39 +0530

    Updated backend and added k8s files

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", 
-    credentials: true,                   
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 
